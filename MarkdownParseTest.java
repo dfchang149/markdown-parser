9ea2fce68d273ab6b263b3b8adfc544398445f59
dfchang149 <dfchang@ucsd.edu>
2022-04-22 12:42:33 -0700

    Part 3
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,13 +10,4 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
-    @Test
-    public void testGettingLinks() throws IOException{
-        Path fileName = Path.of("C:/Users/chang/OneDrive/Documents/GitHub/markdown-parser/test-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = new MarkdownParse().getLinks(content);
-        String[] expected = {"[https://something.com","some-page.html]"};
-	    assertEquals(List.of("https://something.com","some-thing.html"),links); 
-    }
 }
