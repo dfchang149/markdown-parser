ef42ffa111aeefac914ad81759641a3558012628
dfchang149 <dfchang@ucsd.edu>
2022-04-24 22:01:10 -0700

    Reset to previous for Lab Report 2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,24 +30,8 @@ public class MarkdownParse {
                 }
             }
             
-            if(openBracket == -1 || closeBracket == -1 || openParen == -1) {
-                currentIndex = markdown.length();
-                break;
-            } else if ((imageSyntax != -1) && imageSyntax == openBracket-1){ // skips if is image
-                currentIndex = openBracket+1;
-            } else if (openParen != closeBracket+1) { // skips if parenthesis not right after close bracket
-                currentIndex = closeBracket+1;
-            } else if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
-                // skips if enclosed by quotes
-                currentIndex = closeQuotes+1;
-            } else {
-                if (openParen != -1 && closeParen != -1) {
-                    toReturn.add(markdown.substring(openParen + 1, closeParen));
-                    currentIndex = closeParen + 1;
-                } else {
-                    break;
-                }
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
 
         return toReturn;
