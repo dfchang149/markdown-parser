e1c48e5f661b4a0168088cf9954df51c32a667cf
dfchang149 <dfchang@ucsd.edu>
2022-04-24 22:20:00 -0700

    Fix bugs for test files 2-4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,7 +30,10 @@ public class MarkdownParse {
                 }
             }
             
-            if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
+            if(openBracket == -1 || closeBracket == -1 || openParen == -1) { // avoid infinite loops
+                currentIndex = markdown.length();
+                break;
+            } else if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
                 // skips if enclosed by quotes
                 currentIndex = closeQuotes+1;
             }
