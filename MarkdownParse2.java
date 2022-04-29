60edbcf571711a86779215acc077f82b55b77b91
dfchang149 <dfchang@ucsd.edu>
2022-04-29 12:23:42 -0700

    Add Github Actions

---
+++ MarkdownParse2.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import javax.management.openmbean.OpenDataException;
+
+public class MarkdownParse2 {
+    
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+
+        while(currentIndex < markdown.length()) {
+
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            int ExMark = markdown.indexOf("!", openBracket-1);
+            currentIndex = closeParen + 1;
+
+            if(openBracket == -1 || openParen == -1 || closeParen == -1){
+                break;
+            } else if( ExMark == openBracket -1){
+                currentIndex = openBracket + 1;
+            } else if(openParen != closeBracket -1){
+                currentIndex = closeBracket + 1;
+            }
+            else{
+                toReturn.add(markdown.substring(openParen +1, closeParen));
+            }
+
+        }
+        return toReturn;
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+   	    System.out.println(links);         
+
+    }
+}
